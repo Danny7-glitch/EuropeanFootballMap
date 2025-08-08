4bb83564c293a645402184871cd4d09101e91000
Danny7-glitch <59000488+Danny7-glitch@users.noreply.github.com>
2025-08-08 02:03:58 -0400

    Update clubs.js

--- clubs.js
+++ clubs.js
@@ -1,20 +1,18 @@
 const clubs = [
-  {
-    name: "Manchester City",
-    coords: [53.483959, -2.200294],
-    group: "A",
-    country: "England"
-  },
-  {
-    name: "Real Madrid",
-    coords: [40.4531, -3.6883],
-    group: "A",
-    country: "Spain"
-  },
-  {
-    name: "Bayern Munich",
-    coords: [48.2188, 11.6247],
-    group: "B",
-    country: "Germany"
-  }
+  { name: "Manchester City", coords: [53.483959, -2.200294], group: "A", country: "England" },
+  { name: "Real Madrid", coords: [40.4531, -3.6883], group: "A", country: "Spain" },
+  { name: "Bayern Munich", coords: [48.2188, 11.6247], group: "B", country: "Germany" },
+  { name: "Paris Saint-Germain", coords: [48.8414, 2.2530], group: "B", country: "France" },
+  { name: "Inter Milan", coords: [45.4781, 9.1240], group: "C", country: "Italy" },
+  { name: "FC Barcelona", coords: [41.3818, 2.154007], group: "C", country: "Spain" },
+  { name: "Arsenal", coords: [51.5550, -0.1086], group: "D", country: "England" },
+  { name: "Benfica", coords: [38.7520, -9.1847], group: "D", country: "Portugal" },
+  { name: "RB Leipzig", coords: [51.3457, 12.3731], group: "E", country: "Germany" },
+  { name: "Feyenoord", coords: [51.8939, 4.5230], group: "E", country: "Netherlands" },
+  { name: "Galatasaray", coords: [41.0256, 28.9744], group: "F", country: "Turkey" },
+  { name: "Celtic", coords: [55.8497, -4.2055], group: "F", country: "Scotland" },
+  { name: "Club Brugge", coords: [51.2085, 3.2199], group: "G", country: "Belgium" },
+  { name: "Red Bull Salzburg", coords: [47.8095, 13.0550], group: "G", country: "Austria" },
+  { name: "Shakhtar Donetsk", coords: [50.4501, 30.5234], group: "H", country: "Ukraine" },
+  { name: "Copenhagen", coords: [55.6731, 12.5683], group: "H", country: "Denmark" }
 ];
